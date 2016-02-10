4f5fd8aa1dfbd4c60c6ea411fbbb4056514ed656
Daniel Gruenig <dcgruenig@gmail.com>
2016-02-10 22:20:13 +0000

    Added title to database and field in form

--- flashcards/app/controllers/cards_controller.rb
+++ flashcards/app/controllers/cards_controller.rb
@@ -69,6 +69,6 @@ class CardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def card_params
-      params.require(:card).permit(:id, :side_a_text, :side_b_text, :side_a_math, :side_b_math, :side_a_image, :side_b_image, :card_back_color, :card_text_color, :deck_id)
+      params.require(:card).permit(:id, :title, :side_a_text, :side_b_text, :side_a_math, :side_b_math, :side_a_image, :side_b_image, :card_back_color, :card_text_color, :deck_id)
     end
 end
