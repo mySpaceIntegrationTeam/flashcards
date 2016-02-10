c6b3ee4b7040c9507edbd5396f626f806f2bbbe5
Daniel Gruenig <dcgruenig@gmail.com>
2016-02-10 21:57:43 +0000

    Added database entries and created visuals

--- flashcards/db/migrate/20160203052349_create_cards.rb
+++ flashcards/db/migrate/20160203052349_create_cards.rb
@@ -1,10 +1,8 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.integer :card_id
-      t.text :background_color
-      t.text :content
-
+      t.string :card_back_color
+      t.string :card_text_color
       t.timestamps null: false
     end
   end
