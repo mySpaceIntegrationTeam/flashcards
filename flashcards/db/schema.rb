77fe8ba67173d60347b20c05e78714225efdc083
Daniel Gruenig <dcgruenig@gmail.com>
2016-02-13 22:10:09 +0000

    Made integration changes

--- flashcards/db/schema.rb
+++ flashcards/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210221348) do
+ActiveRecord::Schema.define(version: 20160211034604) do
 
   create_table "cards", force: :cascade do |t|
     t.datetime "created_at",      null: false
@@ -43,14 +43,9 @@ ActiveRecord::Schema.define(version: 20160210221348) do
     t.integer  "downloads"
     t.integer  "views"
     t.integer  "flag_id"
-<<<<<<< HEAD
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
-=======
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
     t.string   "category"
->>>>>>> c431b8df3235447faf20b17ae7d75c7ac0a5e071
   end
 
 end
