236bf2c654d408804287a547df5036d43d68a917
lgotliebson <lgotliebson@student.cvtc.edu>
2016-02-16 22:49:24 +0000

    Syncing migrations v3

--- flashcards/app/models/card.rb
+++ flashcards/app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
-  validates :card_title, :side_a_text, :side_b_text, :card_back_color, :card_text_color, :presence => true
+  validates :side_a_text, :side_b_text, :card_back_color, :card_text_color, :presence => true
 end
