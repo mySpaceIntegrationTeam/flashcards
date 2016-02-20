d56a381d6e03f5520c387af971aa623bdce4a201
lgotliebson <lgotliebson@student.cvtc.edu>
2016-02-20 04:26:36 +0000

    removing duplicate columns in db, migrations

--- flashcards/db/migrate/20160201220038_create_decks.rb
+++ flashcards/db/migrate/20160201220038_create_decks.rb
@@ -3,14 +3,8 @@ class CreateDecks < ActiveRecord::Migration
     create_table :decks do |t|
       t.string :title
       t.text :description
-      t.integer :creator_id
-      t.date :published
-      t.integer :category_id
-      t.integer :language_id
-      t.date :updated
       t.string :back_color
       t.string :text_color
-      t.integer :current_contributer_id
       t.integer :downloads
       t.integer :views
       t.integer :flag_id
--- flashcards/db/migrate/20160216200529_add_cart_title_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddCartTitleToCards < ActiveRecord::Migration
-  def change
-    add_column :cards, :card_title, :string
-  end
-end
