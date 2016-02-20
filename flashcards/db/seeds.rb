59d337e8039ee1e9230407f7cb8eeaf7bdbb81d5
lgotliebson <lgotliebson@student.cvtc.edu>
2016-02-20 05:59:48 +0000

    added to seed.db

--- flashcards/db/seeds.rb
+++ flashcards/db/seeds.rb
@@ -9,6 +9,8 @@
 #
 # ruby encoding: utf-8
 
+deck1 = Deck.create(title: 'Ridiculous', description: 'Full of silliness', back_color: 'White', text_color: 'Black', creator: 'I M Idiot', category: 'Misc', language: 'English').
+cards.create(card_title: 'A Joke', card_back_color: 'White', card_text_color: 'Brown', side_a_text: 'Why do the Vikings suck?', side_b_text: 'They are not the Packers!')
 
 
 deck2 = Deck.create(title: 'Sublime', description: 'Full of seriousness', back_color: 'White', text_color: 'Black', creator: 'I M Genius', category: 'Misc', language: 'English').
